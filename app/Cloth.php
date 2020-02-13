36689e607ff6d29a12e76fc52d051fc0395af007
Renzo Carara <renzo.carara@libero.it>
2020-02-13 18:30:56 +0100

    per risolvere il problema del plurale nome_modello vs nome_tabella, aggiunta una linea nella classe Cloth: protected $table = 'clothes', dove 'forzo' il nome della tabella a quello che voglio io

--- app/Cloth.php
+++ app/Cloth.php
@@ -4,9 +4,7 @@
 // NOTA: siccome Laravel non sembra costruire bene il plurale del nome del modello Cloth,
 // gli dico io il nome della tabella da cercare nel DB aggiungendo la seguente linea:
 // protected $table = "clothes";
-// nel file Model.php nella cartella
-// qui sotto:
-// vendor\laravel\framework\src\Illuminate\Database\Eloquent\Model.php
+
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
