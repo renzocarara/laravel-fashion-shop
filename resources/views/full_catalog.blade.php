3fab47619b2df593fe7303903d14f980685ae81a
Renzo Carara <renzo.carara@libero.it>
2020-02-14 11:33:22 +0100

    corretta notazione di accesso ai dati arrivati da DB: usato -> e non square notation

--- resources/views/full_catalog.blade.php
+++ resources/views/full_catalog.blade.php
@@ -22,13 +22,13 @@
                     <div class="card col-lg-3 border-danger" style="width: 18rem;">
                         {{-- <img src="..." class="card-img-top" alt="..."> --}}
                         <div class="card-body text-info">
-                            <h5 class="card-title">{{ $prodotto['name'] }}</h5>
-                            <p class="card-text">{{ $prodotto['description'] }}</p>
+                            <h5 class="card-title">{{ $prodotto->name }}</h5>
+                            <p class="card-text">{{ $prodotto->description }}</p>
                         </div>
                         <ul class="list-group list-group-flush">
-                            <li class="list-group-item">categoria: {{ $prodotto['category'] }}</li>
+                            <li class="list-group-item">categoria: {{ $prodotto->category }}</li>
                             {{-- <li class="list-group-item">disponibilità: </li> --}}
-                            <li class="list-group-item">prezzo: {{ $prodotto['price'] }} €</li>
+                            <li class="list-group-item">prezzo: {{ $prodotto->price }} €</li>
                         </ul>
                         <div class="card-body">
                             <a href="#" class="card-link">Inserisci nel carrello</a>
